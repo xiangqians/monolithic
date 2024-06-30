bef5863091a5afa83864d2f566da6ab0a5fac8ee
xiangqians <shixiangqian.s@gmail.com>
2024-06-30 10:39:44 +0800

    新增sse模块

---
+++ sse/src/main/java/org/xiangqian/monolithic/sse/SseApplication.java
@@ -0,0 +1,8 @@
+package org.xiangqian.monolithic.sse;
+
+/**
+ * @author xiangqian
+ * @date 10:35 2024/06/30
+ */
+public class SseApplication {
+}
---
+++ webrtc/src/test/java/org/xiangqian/monolithic/webrtc/WebrtcTest.java
@@ -0,0 +1,8 @@
+package org.xiangqian.monolithic.webrtc;
+
+/**
+ * @author xiangqian
+ * @date 10:36 2024/06/30
+ */
+public class WebrtcTest {
+}
