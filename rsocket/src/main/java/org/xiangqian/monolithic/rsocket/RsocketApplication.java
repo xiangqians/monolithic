e89ae01b1e5da7558c15a04abf96b0fd41107a3b
xiangqians <shixiangqian.s@gmail.com>
2024-06-30 12:15:32 +0800

    创建rsocket模块

---
+++ rsocket/src/main/java/org/xiangqian/monolithic/rsocket/RsocketApplication.java
@@ -0,0 +1,11 @@
+package org.xiangqian.monolithic.rsocket;
+
+/**
+ * @author xiangqian
+ * @date 12:11 2024/06/30
+ */
+public class RsocketApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
