fa065aa13ce200284294cffac9ae6fc7110c6d5a
xiangqians <shixiangqian.s@gmail.com>
2024-06-30 12:41:20 +0800

    移除rsocket模块

--- common/common-rsocket/src/main/java/org/xiangqian/monolithic/rsocket/Rsocket.java
+++
@@ -1,8 +0,0 @@
-package org.xiangqian.monolithic.rsocket;
-
-/**
- * @author xiangqian
- * @date 12:00 2024/06/30
- */
-public class Rsocket {
-}
--- common/common-rsocket/src/test/java/org/xiangqian/monolithic/rsocket/RsocketTest.java
+++
@@ -1,8 +0,0 @@
-package org.xiangqian.monolithic.rsocket;
-
-/**
- * @author xiangqian
- * @date 12:01 2024/06/30
- */
-public class RsocketTest {
-}
